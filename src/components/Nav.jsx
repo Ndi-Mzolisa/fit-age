5b4ffaddcf5624551d9e094bf4e28ddd2e749e66
Ndi-Mzolisa <ndimandemzolisa@gmail.com>
2024-07-25 14:13:02 +0200

    added new and down sixed logo image

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import fitAgeLogo from '../assets/fit-age-logo.png'
+import fitAgeLogoImage from '../assets/fit-age-logo-image.png'
 
 export default function Nav() {
   return (
-    <div className="py-10 px-4 pb-32 w-full md:w-1/2 md:px-20">
-        <img src={fitAgeLogo} alt="fit-age logo image" width="auto" height="auto"/>
+    <div className="py-10 px-4 pb-32 w-full md:w-1/2 md:px-20 rounded-[0.5rem]">
+        <img src={fitAgeLogoImage} alt="fit-age logo image" width="auto" height="auto" className="rounded-[0.5rem]"/>
     </div>
   )
 }
